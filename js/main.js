c9f46b179e2d9cead732f17b7f6b4467a66df390
JackBDu <jd2804@nyu.edu>
2015-01-28 19:52:19 +0800

    synced with master

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-var UNIT = 20;
-var CANVAS_WIDTH = 24 * UNIT;
-var CANVAS_HEIGHT = 16 * UNIT;
+var UNIT = 30;
+var CANVAS_WIDTH = 16 * UNIT;
+var CANVAS_HEIGHT = 12 * UNIT;
 var score = 0;
+var bestScore = 0;
 var canvasElement = $("<canvas width='" + CANVAS_WIDTH + 
                       "' height='" + CANVAS_HEIGHT + "'>Your browser does not support the HTML5 canvas tag.</canvas>");
 var canvas = canvasElement.get(0).getContext("2d");
 var FPS = 30;
+var paused = false;
 $(document).ready(function(){
-	canvasElement.appendTo('body'); // create canvas in body
+	canvasElement.appendTo('#content_container'); // create canvas in body
 	setInterval(function() {
 		update();
 		draw();
 	}, 1000/FPS); // set FPS 
 });
-var period = 20;
+// var period = 20;
+var period = 3;
 var moveLeft;
 var moveRight;
 var moveUp;
@@ -29,35 +32,41 @@ function move(){
     }
   });
   // check keys
-  if ((keydown.left || swipeDirection == "left") && (currentDirection != "right" || bodyNum == 0)) {
+  if ((keydown.left || keydown.a || swipeDirection == "left") && (currentDirection != "right" || bodyNum == 0)) {
     moveLeft = true;
     moveRight = false;
     moveUp = false;
     moveDown = false;
     swipeDirection = "left";
+    paused = false;
   }
-  if ((keydown.right || swipeDirection == "right") && (currentDirection != "left" || bodyNum == 0)) {
+  if ((keydown.right || keydown.d || swipeDirection == "right") && (currentDirection != "left" || bodyNum == 0)) {
     moveLeft = false;
     moveRight = true;
     moveUp = false;
     moveDown = false;
     swipeDirection = "right";
+    paused = false;
   }
-  if ((keydown.up || swipeDirection == "up") && (currentDirection != "down" || bodyNum == 0)) {
+  if ((keydown.up || keydown.w || swipeDirection == "up") && (currentDirection != "down" || bodyNum == 0)) {
     moveLeft = false;
     moveRight = false;
     moveUp = true;
     moveDown = false;
     swipeDirection = "up";
+    paused = false;
   }
-  if ((keydown.down || swipeDirection == "down") && (currentDirection != "up" || bodyNum == 0)) {
+  if ((keydown.down || keydown.s || swipeDirection == "down") && (currentDirection != "up" || bodyNum == 0)) {
     moveLeft = false;
     moveRight = false;
     moveUp = false;
     moveDown = true;
     swipeDirection = "down";
+    paused = false;
   }
-  
+  if (keydown.space) {
+    paused = !paused;
+  }  
 //   bodies[0] = new body(100,100);
 //   for (var i=0; i<bodyNum; i++) {
 //   	function(){bodies[i] = new body();}
@@ -80,18 +89,21 @@ function move(){
   
   	timer = period;
 //   	body(head.x - UNIT/2, head.y - UNIT/2);
-    if (moveLeft) {
-      currentDirection = "left";
-      head.x -= UNIT;
-    } else if (moveRight) {
-      currentDirection = "right";
-      head.x += UNIT;
-    } else if (moveUp) {
-      currentDirection = "up";
-      head.y -= UNIT;
-    } else if (moveDown) {
-      currentDirection = "down";
-      head.y += UNIT;
+
+    if (!paused) {
+      if (moveLeft) {
+        currentDirection = "left";
+        head.x -= UNIT;
+      } else if (moveRight) {
+        currentDirection = "right";
+        head.x += UNIT;
+      } else if (moveUp) {
+        currentDirection = "up";
+        head.y -= UNIT;
+      } else if (moveDown) {
+        currentDirection = "down";
+        head.y += UNIT;
+      }
     }
   }
   timer--;
@@ -122,6 +134,9 @@ function checkGot() {
   }
   if (got) {
     score++;
+    if (score >= bestScore) {
+      bestScore = score;
+    }
     bodyNum++;
     food.x = UNIT*Math.floor(Math.random()*(CANVAS_WIDTH/UNIT-1))+UNIT*0.5;
     food.y = UNIT*Math.floor(Math.random()*(CANVAS_HEIGHT/UNIT-1))+UNIT*0.5;
@@ -146,47 +161,50 @@ var die = false;
 function checkDie(){
   if (!die) {
     for (var i=0; i<bodyNum;i++) {
-	  if (bodies[i].x==head.x-UNIT/2 && bodies[i].y==head.y-UNIT/2) {
-        die = true;
-        console.log("die");
-  	  }
+  	  if (bodies[i].x==head.x-UNIT/2 && bodies[i].y==head.y-UNIT/2) {
+          die = true;
+          paused = true;
+          document.getElementById("game_over").style.display = "block";
+    	}
     }
   }
 }
 
 // update
 function update() {
-  move();
-  edge();
-  checkGot();
+  if (!die) {
+    updateScores();
+    move();
+    edge();
+    checkGot();
+  }
   checkDie();
 }
 
-function body(tempX,tempY) {
-  this.x = tempX;
-  this.y = tempY;
-  this.draw = function() {
-    canvas.rect(this.x,this.y,UNIT,UNIT);
-    canvas.stroke();
-  };
+function updateScores() {
+  document.getElementById("score").innerHTML = score;
+  document.getElementById("best").innerHTML = bestScore;
+
 }
 
 // draw
 function draw() {
   canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-//	canvas.fillStyle = "#000";
+  canvas.lineWidth = 4;
+  canvas.strokeStyle = '#0d47a1';
+	// canvas.fillStyle = "#000";
 //	canvas.fillText("Sup Bro!", textX, textY);
-  if (!die){
-	food.draw();
-	head.draw();
-	for (var i=0; i<bodyNum;i++) {
-	  bodies[i].draw();
+  // if (!die){
+  	food.draw();
+  	head.draw();
+  	for (var i=0; i<bodyNum;i++) {
+  	  bodies[i].draw();
 	}
-  } else {
-    canvas.fillStyle = "#000";
-    canvas.fillText("SCORE:"+score, CANVAS_WIDTH/2, CANVAS_HEIGHT/2-10);
-    canvas.fillText("GAME OVER!", CANVAS_WIDTH/2, CANVAS_HEIGHT/2);
-  }
+  // } else {
+  //   canvas.fillStyle = "#0d47a1";
+  //   canvas.fillText("SCORE:"+score, CANVAS_WIDTH/2, CANVAS_HEIGHT/2-10);
+  //   canvas.fillText("GAME OVER!", CANVAS_WIDTH/2, CANVAS_HEIGHT/2);
+  // }
 }
 
 var head = {
@@ -195,17 +213,55 @@ var head = {
   y: UNIT*Math.floor(Math.random()*(CANVAS_HEIGHT/UNIT-1))+UNIT*0.5,
   r: UNIT/2,
   draw: function() {
-//     canvas.fillStyle = this.color;
-    canvas.beginPath();
-    canvas.arc(this.x,this.y,this.r, 0,2*Math.PI);
-    canvas.stroke();
+    // canvas.fillStyle = this.color;
+    // canvas.beginPath();
+    // canvas.arc(this.x,this.y,this.r, 0,2*Math.PI);
+    // canvas.stroke();
+    rectX = this.x-UNIT/2;
+    rectY = this.y-UNIT/2;
+    cornerRadius = 8;
+    // canvas.rect(this.x-UNIT/2,this.y-UNIT/2,UNIT,UNIT);
+    // canvas.stroke();
     canvas.beginPath();
     canvas.arc(this.x,this.y,1, 0,2*Math.PI);
+    canvas.stroke();
+
+    canvas.beginPath();
+    canvas.moveTo(rectX + cornerRadius, rectY);
+    canvas.lineTo(rectX + UNIT - cornerRadius, rectY);
+    canvas.arcTo(rectX + UNIT, rectY, rectX + UNIT, rectY + cornerRadius, cornerRadius);
+    canvas.lineTo(rectX + UNIT, rectY + UNIT - cornerRadius);
+    canvas.arcTo(rectX + UNIT, rectY + UNIT, rectX + UNIT - cornerRadius, rectY + UNIT, cornerRadius);
+    canvas.lineTo(rectX + cornerRadius, rectY + UNIT);
+    canvas.arcTo(rectX, rectY + UNIT, rectX, rectY + UNIT - cornerRadius, cornerRadius);
+    canvas.lineTo(rectX, rectY + cornerRadius);
+    canvas.arcTo(rectX, rectY, rectX + cornerRadius, rectY, cornerRadius);
     canvas.stroke();
   }
 };
 
-
+function body(tempX,tempY) {
+  this.x = tempX;
+  this.y = tempY;
+  this.draw = function() {
+    rectX = this.x;
+    rectY = this.y;
+    cornerRadius = 8;
+    canvas.beginPath();
+    canvas.moveTo(rectX + cornerRadius, rectY);
+    canvas.lineTo(rectX + UNIT - cornerRadius, rectY);
+    canvas.arcTo(rectX + UNIT, rectY, rectX + UNIT, rectY + cornerRadius, cornerRadius);
+    canvas.lineTo(rectX + UNIT, rectY + UNIT - cornerRadius);
+    canvas.arcTo(rectX + UNIT, rectY + UNIT, rectX + UNIT - cornerRadius, rectY + UNIT, cornerRadius);
+    canvas.lineTo(rectX + cornerRadius, rectY + UNIT);
+    canvas.arcTo(rectX, rectY + UNIT, rectX, rectY + UNIT - cornerRadius, cornerRadius);
+    canvas.lineTo(rectX, rectY + cornerRadius);
+    canvas.arcTo(rectX, rectY, rectX + cornerRadius, rectY, cornerRadius);
+    canvas.stroke();
+    // canvas.rect(this.x,this.y,UNIT,UNIT);
+    // canvas.stroke();
+  };
+}
 
 var food = {
 //   color: "#000",
@@ -229,20 +285,17 @@ var food = {
 
 // button controls
 $(document).ready(function(){
-  $("#simple").click(function(){
+  $("#easy").click(function(){
     period = 15;
-  });
-  $("#medium").click(function(){
-    period = 10;
+    document.getElementById("difficulties").style.display = "none";
   });
   $("#hard").click(function(){
     period = 5;
+    document.getElementById("difficulties").style.display = "none";
   });
   $("#insane").click(function(){
     period = 3;
-  });
-  $("#crazy").click(function(){
-    period = 1;
+    document.getElementById("difficulties").style.display = "none";
   });
   $("#restart").click(function(){
     die = false;
@@ -253,6 +306,8 @@ $(document).ready(function(){
     moveLeft = false;
     moveDown = false;
     swipeDirection = " ";
+    document.getElementById("game_over").style.display = "none";
+    document.getElementById("difficulties").style.display = "block";
   });
   $("#increaseWidth").click(function(){
     CANVAS_WIDTH += UNIT*2;
@@ -271,3 +326,12 @@ $(document).ready(function(){
     $("canvas").attr("height",CANVAS_HEIGHT);
   });
 });
+
+// prevent scrolling with arrow keys
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var keyCode = evt.keyCode;
+    if (keyCode >= 37 && keyCode <= 40) {
+        return false;
+    }
+};
